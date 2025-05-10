6d7fd7c891ec64e4eeadf55da8d2617db68dccc9
apernea <apernea31@gmail.com>
2025-05-10 13:21:26 +0300

    Am introdus întregul pachet java.util și java.util.concurrent deoarece am nevoie de ambele și astfel am acces oricând la toate funcțiile disponibile.

--- src/main/java/com/siemens/internship/ItemService.java
+++ src/main/java/com/siemens/internship/ItemService.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.*;
 
 @Service
