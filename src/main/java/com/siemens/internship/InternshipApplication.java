86ca1203b099b2e309f6f21f1b4164755615e202
apernea <apernea31@gmail.com>
2025-05-10 12:33:52 +0300

    Am adăugat parametrul scanBasePackages în adnotarea SpringBootApplication pentru a mă asigura că toate componentele, serviciile și bean-urile din pachet sunt scanate corect de Spring și înregistrate în aplicație.

--- src/main/java/com/siemens/internship/InternshipApplication.java
+++ src/main/java/com/siemens/internship/InternshipApplication.java
@@ -3,7 +3,7 @@ package com.siemens.internship;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.siemens.internship")
 public class InternshipApplication {
 
 	public static void main(String[] args) {
