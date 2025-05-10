b4c5d49e6720b2747806f5a584414f0a65f26ba8
apernea <apernea31@gmail.com>
2025-05-10 13:01:00 +0300

    Am renunțat la import din cauza modificării anterioare cu Constructor Injection

--- src/main/java/com/siemens/internship/ItemController.java
+++ src/main/java/com/siemens/internship/ItemController.java
@@ -1,7 +1,6 @@
 package com.siemens.internship;
 
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
