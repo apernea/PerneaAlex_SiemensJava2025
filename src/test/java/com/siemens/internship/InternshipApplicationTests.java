cc9b6bfaa97d61c34ce2ee3dedaea8b9434209a7
apernea <apernea31@gmail.com>
2025-05-10 13:50:11 +0300

    Am creat unit test-uri pentru celelalte operații CRUD, cât și metodei de procesare asincronă ca să le pot asigura funcționalitatea corectă. De asemenea, am creat o metodă care să golească database-ul creat de fiecare test pentru a preveni erori în momentul execuției celorlalte teste.

--- src/test/java/com/siemens/internship/InternshipApplicationTests.java
+++ src/test/java/com/siemens/internship/InternshipApplicationTests.java
@@ -44,4 +44,69 @@ class InternshipApplicationTests {
 				.andExpect(status().isCreated());
 	}
 
+	@Test
+	void testGetAllItems() throws Exception {
+		mockMvc.perform(get("/api/items"))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	void testGetItemById() throws Exception {
+		Item item = new Item("test item", "description",
+				"UNPROCESSED","apernea31@gmail.com");
+		item = itemRepository.save(item);
+
+		mockMvc.perform(get("/api/items/" + item.getId()))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	void testUpdateItem() throws Exception {
+		Item existingItem = new Item("existing item", "description",
+				"NEW", "apernea31@gmail.com");
+		existingItem = itemRepository.save(existingItem);
+
+		Item updatedItem = new Item("updated item", "new description",
+				"PROCESSED", "apernea31@gmail.com");
+
+		mockMvc.perform(put("/api/items/" + existingItem.getId())
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(mapper.writeValueAsString(updatedItem)))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	void testDeleteItem() throws Exception {
+		Item item = new Item("test item", "description",
+				"UNPROCESSED","apernea31@gmail.com");
+		item = itemRepository.save(item);
+
+		mockMvc.perform(delete("/api/items/" + item.getId()))
+				.andExpect(status().isNoContent());
+	}
+
+	@Test
+	void testProcessedItems() throws Exception {
+		Item item1 = new Item("item1","test item 1",
+				"UNPROCESSED","apernea31@gmail.com");
+		Item item2 = new Item("item2","test item 2",
+				"UNPROCESSED","apernea31@gmail.com");
+
+		itemRepository.save(item1);
+		itemRepository.save(item2);
+
+		CompletableFuture<List<Item>> future = itemService.processItemsAsync();
+		List<Item> processedItems = future.get();
+		assertEquals(2, processedItems.size());
+		assertTrue(processedItems.stream().allMatch(item ->"PROCESSED".equals(item.getStatus())));
+
+		List<Item> recordedItems = itemRepository.findAll();
+		assertTrue(recordedItems.stream().allMatch(item -> "PROCESSED".equals(item.getStatus())));
+
+	}
+
+	@AfterEach
+	void clearDatabase() {
+		itemRepository.deleteAll();
+	}
 }
