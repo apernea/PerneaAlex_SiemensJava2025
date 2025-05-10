b69dd4a0fbbe8da2465736ee5113bd0d6d692856
apernea <apernea31@gmail.com>
2025-05-10 12:41:24 +0300

    Am adăugat librăriile necesare.

--- src/main/java/com/siemens/internship/Item.java
+++ src/main/java/com/siemens/internship/Item.java
@@ -1,14 +1,9 @@
 package com.siemens.internship;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
 
 @Entity
 @Data
