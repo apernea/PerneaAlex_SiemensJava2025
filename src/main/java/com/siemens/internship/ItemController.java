daabd303d7132fcd1e25a313d4196f7cd9a5a5ba
apernea <apernea31@gmail.com>
2025-05-10 13:01:37 +0300

    Am renunțat la acest import odată cu modificarea metodei createItem()

--- src/main/java/com/siemens/internship/ItemController.java
+++ src/main/java/com/siemens/internship/ItemController.java
@@ -3,7 +3,6 @@ package com.siemens.internship;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
